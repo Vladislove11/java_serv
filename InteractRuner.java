9357b680e3211528e08825c3aeec9fbbfe67529b
Vladislove11 <Vladislove11@gmail.com>
2020-02-27 23:18:15 +0300

    user imput

---
+++ InteractRuner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class InteractRunner{
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        try{
+            calculator calc = new Calculator();
+            String exit = "no";
+            while(!exit.equals("Yes")){
+                System.out.println("Enter first arg : ");
+                String first = reader.next();
+                
+            }
+        }
+    }
+}
--- calculate.java
+++ calculate.java
@@ -1,5 +1,21 @@
 public class calculate{
+	private int rezult;
+
+	public void add(int ... params){
+		for(Integer param : params){
+			this.rezult += param;
+		}
+	}
+
+	public int getRezult(){
+		return this.rezult;
+	}
+
+	public void cleanRezult(){
+		this.rezult = 0;
+	}
 	public static void main(String[] arg){
+
 		System.out.println("calculate");
 	}
 }
